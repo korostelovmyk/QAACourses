884ce271b85837ac91727f88071c48c6aa0bda1b
Mykhailo Korostelov <korostelev.mihail@gmail.com>
2025-10-30 18:06:35 +0000

    Moved test cases from main folder to dedicated.

---
+++ QAACourses/Drivers/DriverFactory.cs
@@ -0,0 +1,44 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace QAACourses.Drivers
+{
+    public class DriveFactory
+    {
+        public static IWebDriver CreateDriver()
+        {
+            var options = new ChromeOptions();
+            // options.AddArgument("--headless"); 
+           
+            var driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            
+            return driver;
+        }
+        /*
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        private const string _url = "https://theconnectedshop.uk";
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+            // options.AddArgument("--headless");
+
+            _driver = new ChromeDriver(options);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            _driver.Navigate().GoToUrl(_url);
+
+
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _driver.Dispose();
+        }*/
+    }
+}
